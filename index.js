1f7d808558934c5b55b13f3ab18096e38a269668
mnapearson <marratoon@gmail.com>
2020-09-15 16:57:48 +0200

    add post and get functions

--- index.js
+++ index.js
@@ -12,3 +12,39 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send({ msg: "Hello World" });
 });
+
+app.get("/users/:id/events", async (req, res) => {
+  try {
+    res.send(await knex("events").select().where({ userId: req.params.id }));
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/users/:id/tasks", async (req, res) => {
+  try {
+    res.send(await knex("tasks").select().where({ userId: req.params.id }));
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/users/:id/events", async (req, res) => {
+  try {
+    const [id] = await knex("events").insert(req.body);
+    const event = await knex("events").select().where({ id });
+    res.status(201).send(event);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(400);
+  }
+});
+
+async function main() {
+  await knex.migrate.latest();
+  app.listen(PORT, () =>
+    console.log(`Server started listening on http://localhost:${PORT}`)
+  );
+}
+
+main();
